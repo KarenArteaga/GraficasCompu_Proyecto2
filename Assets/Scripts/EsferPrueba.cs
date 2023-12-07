94a0824a27649ffc74d2a9413b631504b6b8b29b
KarenArteaga <305.arteaga.karen@gmail.com>
2023-12-07 16:43:22 -0600

    First commit, empty project and .gitignore

--- Assets/Scripts/EsferPrueba.cs
+++ Assets/Scripts/EsferPrueba.cs
@@ -2,22 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Boton : MonoBehaviour
+public class EsferPrueba : MonoBehaviour
 {
-    private Vector3 pos;
 
+    private Vector3 pos;
+    //[SerializeField] private GameObject figura1;
+    // [SerializeField] private GameObject figura2;
+    [SerializeField] private List<GameObject> listaFiguras1 = new List<GameObject>();
     [SerializeField] private List<GameObject> listaFiguras = new List<GameObject>();
-    [SerializeField] private List<GameObject> listaFigurasInst = new List<GameObject>();
     private GameObject figurasHIER;
 
-    private int sigFig = 0;
-    private int capa = 0;
-
+    // Start is called before the first frame update
     private float[] coords = {
         //  x       z       rotx    roty      rotz
         0.31f, -18.25f,     90.0f,  13.056f, 0.0f,   //fig1
-        3.78f, -18.25f,     90.0f,  22.806f, 0.0f,   //fig2
-        //---- capa 2 ----
+        3.78f, -18.25f,     90.0f,  22.806f, 0.0f,  //fig2
+        //--- capa 2 ---
         0.04f, -18.34f,     0.0f,   0.0f,   0.0f,    //fig21
         0.6f,  -18.34f,     0.0f,   0.0f,   0.0f,    //fig22
         3.797f, -18.77f,    0.0f,   0.0f,   0.0f,    //fig3
@@ -45,7 +45,7 @@ public class Boton : MonoBehaviour
         2.045f, -18.25f,     13.3f,  -90.0f, 0.0f,    //fig19
         3.797f, -18.77f,     0.0f,   0.0f,   0.0f,     //fig9
         //--- capa 9 ---
-        2.045f, -18.25f,     13.53f,  90.0f, 0.0f,    //fig19
+        2.045f, -18.25f,     13.53f,  90.0f, 0.0f,    //fig20
         3.797f, -18.77f,     0.0f,   0.0f,   0.0f,     //fig10
         //--- capa 10 ---
         3.797f, -18.77f,     -90.0f,   0.0f,   0.0f,     //fig11
@@ -54,28 +54,17 @@ public class Boton : MonoBehaviour
         //--- capa 12 ---
         3.797f, -18.77f,     0.0f,   0.0f,   0.0f,    //fig13
         0.28f,  -18.34f,     0.0f,   0.0f,   0.0f     //fig28
+
     };
 
-    public List<GameObject> ListaFiguras
-    {
-        get { return listaFiguras; }
-        set { listaFiguras = value; }
-    }
+    private int indice = 0;
+    private int figura = 0;
 
-    public List<GameObject> ListaFigurasInst
-    {
-        get { return listaFigurasInst; }
-        set { listaFigurasInst = value; }
-    }
-
-    // Start is called before the first frame update
     void Start()
     {
-
         pos = transform.position;
         figurasHIER = new GameObject();
         figurasHIER.name = "Figuras";
-
     }
 
     // Update is called once per frame
@@ -85,45 +74,46 @@ public class Boton : MonoBehaviour
         VerificarAltura();
     }
 
-    void VerificarAltura()
+    void CrearFigura()
     {
-        foreach (GameObject figura in listaFigurasInst)
+        if (Input.GetButtonDown("Jump") && figura < 28)
         {
-            if (figura.transform.position.y < -10)
-                figura.transform.position = pos + new Vector3(0.5f, 5, 0);
+            GameObject enemigoInst = Instantiate(listaFiguras1[figura], new Vector3(coords[indice], 10.0f,
+                coords[indice + 1]), Quaternion.Euler(coords[indice + 2], coords[indice + 3], coords[indice + 4]));
+            //GameObject enemigoInst2 = Instantiate(listaFiguras1[1], new Vector3(coords[5], Random.Range(6.27f, 10.0f),
+             //   coords[6]), Quaternion.Euler(coords[7], coords[8], coords[9]));
+
+            listaFiguras.Add(enemigoInst.gameObject);
+            // listaFiguras.Add(enemigoInst2.gameObject);
+            enemigoInst.transform.parent = figurasHIER.gameObject.transform;
+
+            indice += 5;
+            figura++;
+
         }
     }
 
-    void CrearFigura()
+
+    private void VerificarAltura()
     {
-        if (Input.GetButtonDown("Jump") && sigFig < 28)
+        foreach (GameObject enemigo in listaFiguras)
         {
-            Debug.Log("siFig: " + sigFig + " capa: " + capa);
-            GameObject figInst = Instantiate(listaFiguras[sigFig], new Vector3(coords[capa], 10, coords[capa + 1]),
-                Quaternion.Euler(coords[capa + 2], coords[capa + 3], coords[capa + 4]));
-
-            capa += 5;
-            sigFig++;
-            // GameObject fig2Inst = Instantiate(fig2, new Vector3(3.78f, Random.Range(6.27f, 10.0f), -18.58f), Quaternion.Euler(85.47f, 22.806f, -162.6f));
-
-            listaFigurasInst.Add(figInst.gameObject);
-            figInst.transform.parent = figurasHIER.gameObject.transform;
+            if (enemigo.transform.position.y < -10)
+                enemigo.transform.position = pos + new Vector3(0.5f, 5, 0);
         }
     }
 
     private void OnMouseDown()
     {
-        ExplotarFiguras();
-        Debug.Log("Picaste boton");
+        ExplotarEnemigo();
     }
 
-    public void ExplotarFiguras()
+    public void ExplotarEnemigo()
     {
-        foreach (GameObject figura in listaFigurasInst)
+        foreach (GameObject enemigo in listaFiguras)
         {
-            Debug.Log("Hola");
-            Figuras fig = figura.GetComponent<Figuras>();
-            fig.Explotar(pos);
+            Figuras en = enemigo.GetComponent<Figuras>();
+            en.Explotar(pos);
         }
 
     }
